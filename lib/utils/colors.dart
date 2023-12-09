fff9288805827c533e700b3695fc594510a47ba9
Akshat-Gupta-30 <akshatgupta0030@gmail.com>
2023-12-09 22:59:35 +0530

    Added Constant Colors colors.dart

---
+++ lib/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const backgroundColor = Color.fromRGBO(0, 0, 0, 1);
+const searchColor = Color.fromRGBO(38, 38, 38, 1);
+const blueColor = Color.fromRGBO(0, 149, 246, 1);
+const primaryColor = Color.fromRGBO(255, 255, 255, 1);
+const secondaryColor = Color.fromRGBO(158, 158, 158, 1);
+const notificationColor = Color.fromRGBO(255, 48, 64, 1);
