f6d200111077ee137f0541d9719bb4133574ea87
Akshat-Gupta-30 <akshatgupta0030@gmail.com>
2023-12-09 23:54:46 +0530

    Implemented SignUpScreen UI Implemented SignUpScreen UI with profile image selection, custom text fields, and registration logic. Integrated ImagePicker for image selection and custom snackbar for success message.

---
+++ lib/screens/login_signup/signup/signup.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:instagram/resources/pick_image.dart';
+import 'package:instagram/utils/colors.dart';
+import 'package:instagram/utils/dimensions.dart';
+import 'package:instagram/widgets/custom_button.dart';
+import 'package:instagram/widgets/custom_snackbar.dart';
+import 'package:instagram/widgets/custom_text_field.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  bool isLoading = false;
+
+  Uint8List ? _selectedImage;
+  bool _isImageAdded = false;
+
+  void _loadDefaultImage() async {
+    ByteData data = await rootBundle.load('assets/images/defaultProfilePicture.jpg');
+    setState(() {
+      _selectedImage = data.buffer.asUint8List();
+      _isImageAdded = false;
+    });
+  }
+
+  void _selectImage() async {
+		Uint8List ? img = await pickImage(ImageSource.gallery);
+    setState(() {
+      if (img != null) {
+        _selectedImage = img;
+        _isImageAdded = true;
+      } else {
+        _loadDefaultImage();
+      }
+    });
+	}
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        backgroundColor: blueColor.withOpacity(0.15),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Form(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(height: getScreenHeight(context) * 0.05),
+                    SizedBox(// * : Instagram Logo
+                      height: 50,
+                      width: double.infinity,
+                      child: SvgPicture.asset(
+                        "assets/images/ic_instagram.svg",
+                        colorFilter: const ColorFilter.mode(
+                          primaryColor,
+                          BlendMode.srcIn,
+                        ),
+                        alignment: Alignment.center,
+                      ),
+                    ),
+                    SizedBox(height: getScreenHeight(context) * 0.05),
+                    Stack(// * : Profile Image
+                      children: [
+                        _selectedImage != null
+                        ? CircleAvatar(
+                          radius: 72,
+                          backgroundImage: MemoryImage(_selectedImage!),
+                        )
+                        : const CircleAvatar(
+                          radius: 72,
+                          backgroundImage: AssetImage("assets/images/defaultProfilePicture.jpg"),
+                        ),
+                        if(_isImageAdded)
+                          Positioned(// * : Remove Icon
+                            right: -5,
+                            top: -5,
+                            child: GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  _loadDefaultImage();
+                                });
+                              },
+                              child: const Icon(
+                                Icons.close,
+                                size: 30,
+                              )
+                            ),
+                          ),
+                        Positioned(// * : Camera Icon
+                          right: 0,
+                          bottom: 10,
+                          child: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                _selectImage();
+                              });
+                            },
+                            child: Container(
+                              padding: const EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30),
+                                color: blueColor
+                              ),
+                              child: const Icon(
+                                Icons.camera_alt
+                              ),
+                            )
+                          ),
+                        )
+                      ]
+                    ),
+                    SizedBox(height: getScreenHeight(context) * 0.02),
+                    CustomTextField(// * : Text Field input
+                      controller: usernameController,
+                      keyboardType: TextInputType.text,
+                      textInputAction: TextInputAction.next,
+                      labelText: "Username",
+                    ),
+                    SizedBox(height: getScreenHeight(context) * 0.02),
+                    CustomTextField(// * : Email Input
+                      controller: emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      textInputAction: TextInputAction.next,
+                      labelText: "Email address",
+                    ),
+                    SizedBox(height: getScreenHeight(context) * 0.02),
+                    CustomTextField(// * : Password Input
+                      controller: passwordController,
+                      keyboardType: TextInputType.text,
+                      textInputAction: TextInputAction.done,
+                      labelText: "New Password",
+                    ),
+                    SizedBox(height: getScreenHeight(context) * 0.025),
+                    CustomButtonWithLoading(// * : Login Button
+                      isLoading: isLoading,
+                      onTap: (){
+                        FocusScope.of(context).unfocus();
+                        performFunction(context);
+                      },
+                      labelText: "Sign Up",
+                    ),
+                    SizedBox(height: getScreenHeight(context) * 0.02),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      )
+    );
+  }
+
+  void performFunction(context) async {
+    customSnackbar(context: context, type: "Success", message: "Sign In Successfully");
+  }
+}
