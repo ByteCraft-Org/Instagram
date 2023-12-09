8aabc8f9d9afad6c9c1435fa9c31bb4a56e71197
Akshat-Gupta-30 <akshatgupta0030@gmail.com>
2023-12-09 23:27:15 +0530

    New Widget Custom Text Field custom_text_field     Normal text field     Password text field

---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:instagram/utils/colors.dart';
+import 'package:instagram/utils/dimensions.dart';
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String labelText;
+  final TextInputType keyboardType;
+  final TextInputAction textInputAction;
+  final int maxLines;
+
+  const CustomTextField({
+    super.key, 
+    required this.controller,
+    required this.labelText,
+    this.keyboardType = TextInputType.text,
+    this.textInputAction = TextInputAction.done,
+    this.maxLines = 1,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        maxHeight: getScreenHeight(context) * 0.75,
+      ),
+      child: TextFormField(
+        controller: controller,
+        keyboardType: keyboardType,
+        textInputAction: textInputAction,
+        maxLines: maxLines,
+
+        obscureText: labelText.contains("Password"),
+
+        decoration: InputDecoration(
+          isDense: true,
+          fillColor: Colors.transparent,
+
+          hintText: labelText,
+          hintStyle: const TextStyle(
+            fontSize: 18.0,
+            color: Colors.grey,
+            fontWeight: FontWeight.bold
+          ),
+
+          counterText: "",
+
+          enabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+              color: Colors.grey,
+              width: 2,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+              color: blueColor,
+              width: 3,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+
+          contentPadding: const EdgeInsets.all(15)
+        ),
+      )
+    );
+  }
+}
+
+class CustomPasswordTextField extends StatefulWidget {
+  final TextEditingController controller;
+  final String labelText;
+  final TextInputAction textInputAction;
+
+  const CustomPasswordTextField({
+    super.key, 
+    required this.controller,
+    required this.labelText,
+    this.textInputAction = TextInputAction.done,
+  });
+
+  @override
+  State<CustomPasswordTextField> createState() => _CustomPasswordTextFieldState();
+}
+
+class _CustomPasswordTextFieldState extends State<CustomPasswordTextField> {
+  bool obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        maxHeight: getScreenHeight(context) * 0.75,
+      ),
+      child: TextFormField(
+        controller: widget.controller,
+        keyboardType: TextInputType.text,
+        textInputAction: widget.textInputAction,
+        maxLines: 1,
+
+        obscureText: obscureText,
+
+        decoration: InputDecoration(
+          isDense: true,
+          fillColor: Colors.transparent,
+
+          hintText: widget.labelText,
+          hintStyle: const TextStyle(
+            fontSize: 18.0,
+            color: Colors.grey,
+            fontWeight: FontWeight.bold
+          ),
+
+          counterText: "",
+
+          enabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+              color: Colors.grey,
+              width: 2,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+              color: blueColor,
+              width: 3,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+
+          suffixIcon: IconButton(
+            icon: Icon(
+              obscureText ? Icons.visibility : Icons.visibility_off
+            ),
+            onPressed: () {
+              setState(() {
+                obscureText = !obscureText;
+              });
+            },
+          ),
+
+          contentPadding: const EdgeInsets.all(15)
+        ),
+      )
+    );
+  }
+}
