4e2cc7f8b1428d06271e52c627317711bb020b28
Akshat-Gupta-30 <akshatgupta0030@gmail.com>
2023-12-09 23:01:13 +0530

    Added Constant dimensions dimensions.dart

---
+++ lib/utils/dimensions.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+double getScreenHeight(BuildContext context) => MediaQuery.of(context).size.height;
+double getScreenWidth(BuildContext context) => MediaQuery.of(context).size.width;
