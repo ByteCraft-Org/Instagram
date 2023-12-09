ada70cc7df7469cc42a457b5d46f6ffdb33fff71
Akshat-Gupta-30 <akshatgupta0030@gmail.com>
2023-12-09 23:03:07 +0530

    Added Custom Button widgets custom_button.dart     Normal Button     Loading button     Text Button

---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:instagram/utils/colors.dart';
+
+class CustomButton extends StatelessWidget {
+  final Function() onTap;
+  final String labelText;
+  final Color textColor, buttonColor, borderColor;
+  final bool isDisabled;
+
+  const CustomButton({
+    super.key,
+    required this.onTap,
+    required this.labelText,
+    this.isDisabled = false,
+    this.textColor = primaryColor,
+    this.buttonColor = blueColor,
+    this.borderColor = blueColor
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: isDisabled ? null : onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        decoration: BoxDecoration(
+          color: buttonColor,
+          border: Border.all(
+            width: 1,
+            color: borderColor
+          ),
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Center(
+          child: Text(
+            labelText,
+            style: TextStyle(
+              color: textColor,
+              fontSize: 18,
+              fontWeight: FontWeight.normal
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButtonWithLoading extends StatelessWidget {
+  final Function() onTap;
+  final String labelText;
+  final Color textColor, buttonColor, borderColor;
+  final bool isDisabled;
+  final bool isLoading;
+
+  const CustomButtonWithLoading({
+    super.key,
+    required this.isLoading,
+    required this.onTap,
+    required this.labelText,
+    this.isDisabled = false,
+    this.textColor = primaryColor,
+    this.buttonColor = blueColor,
+    this.borderColor = blueColor
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: isDisabled
+      ? null
+      : isLoading
+        ? null
+        : onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        decoration: BoxDecoration(
+          color: buttonColor,
+          border: Border.all(
+            width: 1,
+            color: borderColor
+          ),
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Center(
+          child: isLoading
+          ? SpinKitThreeBounce(
+            color: textColor,
+            size: 24,
+          )
+          : Text(
+            labelText,
+            style: TextStyle(
+              color: textColor,
+              fontSize: 18,
+              fontWeight: FontWeight.normal
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomTextButton extends StatelessWidget {
+  final Function() onTap;
+  final String labelText;
+  final Color textColor;
+
+  const CustomTextButton({
+    super.key,
+    required this.onTap,
+    required this.labelText,
+    this.textColor = blueColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Text(
+        labelText,
+        style: TextStyle(
+          color: textColor,
+          fontSize: 15,
+          fontWeight: FontWeight.normal
+        ),
+      ),
+    );
+  }
+}
